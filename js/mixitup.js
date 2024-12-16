3d01e10ec778c8411dad99e1ec6aaccb9949a470
Chris Droom <chrisdroom@gmail.com>
2024-12-16 00:26:07 +0000

    misc

---
+++ js/mixitup.js
@@ -0,0 +1,17 @@
+mixitup('#mix-wrapper', {
+  load: {
+  	sort: 'order:asc'
+  },
+	animation: {
+    effects: 'fade',
+    duration: 600
+  },
+  classNames: {
+    block: 'programs',
+    elementFilter: 'filter-btn',
+    elementSort: 'sort-btn'
+  },
+  selectors: {
+    target: '.mix'
+  }
+});
